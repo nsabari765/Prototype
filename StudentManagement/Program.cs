97cb97e33907a101864ac3050a8b6b2690de2db1
nsabari765 <nsabari765@gmail.com>
2024-03-09 12:38:37 +0530

    Student Html Page

--- StudentManagement/Program.cs
+++ StudentManagement/Program.cs
@@ -1,7 +1,13 @@
+using DemoMvc.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DataContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("StudentManagement")));
 
 var app = builder.Build();
 
---
+++ StudentManagement/Repository/StudentRepository.cs
@@ -0,0 +1,16 @@
+﻿using StudentManagement.Models;
+
+namespace StudentManagement.Repository
+{
+    public interface IStudentRepository
+    {
+        Task<Student> GetById(int id);
+    }
+    public class StudentRepository : IStudentRepository
+    {
+        public Task<Student> GetById(int id)
+        {
+            return null;
+        }
+    }
+}
